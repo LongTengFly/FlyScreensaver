7dc7723d46165333644c677793cd99978a5113d4
LongTengFly <ntf_work@163.com>
2022-11-12 22:49:38 +0800

    feat: update

--- SampleScr/SampleScr.cpp
+++ SampleScr/SampleScr.cpp
@@ -1,31 +1,55 @@
 #include <windows.h>
-
+#include "resource.h"
 #pragma comment(lib,"scrnsave.lib") //使用库文件
 
-#include "resource.h"
+#pragma comment(lib,"comctl32.lib") 
 
+// Timer ID
+#define TIMER_ID_FLUSH	101
+
+// Timer Elapse time
+#define TIMER_ELAPSE_500MS	500
+
+// Timer callback
+static void CALLBACK TimerProc(HWND hWnd, UINT nMsg, UINT nTimerid, DWORD dwTime);
 
 LRESULT CALLBACK ScreenSaverProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-
 {
+
+	static BOOL bFlushTimerFlag = TRUE;
 
 	switch (message)
 	{
 
 	case WM_PAINT:
-
 	{
 
 		PAINTSTRUCT ps;
 
-		HDC hDC = BeginPaint(hWnd, &ps);
+		HDC hDC;
+		hDC = BeginPaint(hWnd, &ps);
 
 		SetBkMode(hDC, TRANSPARENT);
 
 		SetTextColor(hDC, RGB(0x33, 0xcc, 0));
 
-		TextOut(hDC, 150, 150, TEXT("WWW.VCKBASE.COM"), 15);
+		TCHAR sztext[128];
+		SYSTEMTIME mCurrentTime;
 
+		GetLocalTime(&mCurrentTime);
+
+		wsprintf(sztext, TEXT("%d-%02d-%02d %02d:%02d:%02d"),
+			mCurrentTime.wYear, mCurrentTime.wMonth, mCurrentTime.wDay,
+			mCurrentTime.wHour, mCurrentTime.wMinute, mCurrentTime.wSecond);
+
+		TextOut(hDC, 150, 150, sztext, 128);
+
+		if (bFlushTimerFlag)
+		{
+			bFlushTimerFlag = FALSE;
+			SetTimer(hWnd, TIMER_ID_FLUSH, TIMER_ELAPSE_500MS, (TIMERPROC)TimerProc);
+		}
+		
 		EndPaint(hWnd, &ps);
 
 	}
@@ -33,7 +57,7 @@ LRESULT CALLBACK ScreenSaverProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
 	break;
 
 	case WM_DESTROY:
-
+		KillTimer(hWnd, TIMER_ID_FLUSH);
 		PostQuitMessage(0);
 
 		break;
@@ -97,4 +121,19 @@ BOOL WINAPI RegisterDialogClasses(HANDLE hInst)
 
 	return TRUE;
 
+}
+
+
+
+static void CALLBACK TimerProc(HWND hWnd, UINT nMsg, UINT nTimerid, DWORD dwTime)
+{
+	switch (nTimerid)
+	{
+	case TIMER_ID_FLUSH:
+		//KillTimer(hWnd, TIMER_ID_FLUSH);
+		InvalidateRect(hWnd, NULL, FALSE);
+		break;
+	default:
+		break;
+	}
 }
